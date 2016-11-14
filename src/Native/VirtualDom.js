864a8eacb175e4b8e7a2ec271a59ffd2d61bfc41
chancyk <chancykennedy@gmail.com>
2016-11-14 16:35:35 -0600

    Handle contenteditable element removal issue.

--- src/Native/VirtualDom.js
+++ src/Native/VirtualDom.js
@@ -865,7 +865,32 @@ function diffChildren(aParent, bParent, patches, rootIndex)
 	{
 		index++;
 		var aChild = aChildren[i];
-		diffHelp(aChild, bChildren[i], patches, index);
+		var bChild = bChildren[i];
+
+        // The following checks on the `text` type elements address issue:
+        //   https://github.com/elm-lang/virtual-dom/issues/48
+        //
+        if (aChild.type == "text" && bChild.type == "text"){
+            if (aChild.text == "" && bChild.text == ""){
+                // Do nothing so that the browser does not remove the cursor
+                // from the element if its a contenteditable element.
+            }
+            else if (aChild.text == "" || bChild.text == ""){
+                // We need to redraw the parent instead of trying to replace
+                // the value of the text element, because Chrome removes
+                // this element entirely once its value is an empty string.
+                patches.push(makePatch('p-redraw', rootIndex, bParent));
+            }
+            else {
+                // Both elements have an actual text value so we'll proceed with
+                // the normal `text` patch.
+                diffHelp(aChild, bChild, patches, index);
+            }
+        }
+        else {
+            diffHelp(aChild, bChild, patches, index);
+        }
+
 		index += aChild.descendantsCount || 0;
 	}
 }
